bfa053d093bc855b0cf71f1d74a94005e90774b1
Chetan Chatwani <chetanchatwani@gmail.com>
2021-04-13 20:53:12 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ var canvas = new fabric.Canvas('myCanvas');
 
 var canvasHeight;
 var canvasWidth;
-var player_x;
-var player_y;
+
+var player_x = 10 ;
+var player_y = 10 ;
+
+var player_object= "";
+var block_image_object= "";
+
+var block_image_width = 30;
+var block_image_height = 30;
 
 var player_object= "";
 var block_image_object= "";
@@ -13,14 +20,15 @@ var block_image_object= "";
 function player_update()
 {
 	fabric.Image.fromURL("images/player.png", function(Img) {
-	player_object = Img;
+		player_object = Img;
 
-	player_object.scaleToWidth(150);
-	player_object.scaleToHeight(140);
-	player_object.set({
-	top:player_y,
-	left:player_x
+		player_object.scaleToWidth(150);
+		player_object.scaleToHeight(140);
+		player_object.set({
+		top:player_y,
+		left:player_x
 	});
+	
 	canvas.add(player_object);
 
 	});
